f7f09e27b9d81f1d811e00f27d81c108f91c122f
Catalina Sabogal <csabogalortiz@gmail.com>
2022-12-13 21:35:44 +0100

    nav bar medio arreglada

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -5,7 +5,6 @@ import Feed from "../pages/Feed/Feed"
 import Discover from "../pages/Discover/Discover"
 import Feeling from "../pages/Feeling/Feeling"
 import Profile from "../pages/Profile/Profile"
-// import Followers from "../pages/Followers/Followers"
 import Community from "../pages/Community/Community"
 import PostDetails from "../pages/PostDetails/PostDetails"
 import NewPost from "../pages/NewPost/NewPost"
@@ -26,7 +25,6 @@ const AppRoutes = () => {
                 <Route path="/feed" element={<Feed />} />
                 <Route path="/profile/:user_id" element={<Profile isOwner={false} />} />
                 <Route path="/myprofile" element={<Profile isOwner={true} />} />
-                {/* <Route path="/profile/:user_id/followers" element={<Followers />} /> */}
                 <Route path="/community" element={<Community />} />
                 <Route path="/posts/:post_id/details" element={<PostDetails />} />
                 <Route path="/posts/create" element={<NewPost />} />
